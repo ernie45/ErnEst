42cfbc57840fda167a3519ed4ae160df9cc878e4
Ernesto Hernanez <h_ernesto@ymail.com>
2022-06-20 14:22:41 -0700

    Initial commit with basic front and back end connectivity

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+/** Require express router */
+const router = require("express").Router();
+/** Require api routes to redefine */
+/** These can be called from api without routing into the actual file */
+/** Because api folder has an index file that exports */
+
+/** This function adds /api to the path of apiRoutes */
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+/** Require express */
+/** And its dependencies */
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+
+/** Define usage of express app */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+/** Define express routing static path */
+/** This is where everything will be directed from */
+/** Without this line, the app doesn't know where to begin */ 
+app.use(express.static("client/build"));
+/** Add the routings to the express app */
+app.use(routes);
+
+/** Use port 3001 or the environment port */
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log("App listening on port " + PORT)
+    console.log(process.env.TD_AUTH);
+});
