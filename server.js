edeeb6c18ba6129e02d48a560bba699891394fff
Ernesto Hernanez <h_ernesto@ymail.com>
2022-06-30 03:31:43 -0700

    changed port once again

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("client/build"));
 app.use(routes);
 
 /** Use port 3001 or the environment port */
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log("App listening on port " + PORT)
